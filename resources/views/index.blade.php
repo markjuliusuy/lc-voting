980fbd8758217469bc063d01376ea0a26930b00e
Mark Julius Uy <markjuliusuy@gmail.com>
2021-11-18 21:52:24 +0800

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<x-app-layout>
+    <div class="filters flex space-x-6">
+        <div class="w-1/3">
+            <select name="category" id="category" class="w-full rounded-xl px-4 py-2">
+                <option value="Category One">Category One</option>
+            </select>
+        </div>
+        <div class="w-1/3">
+            <select name="other_filters" id="other_filters" class="w-full rounded-xl px-4 py-2">
+                <option value="Filter One">Filter One</option>
+            </select>
+        </div>
+        <div class="w-2/3">
+            <select name="other_filters" id="other_filters" class="w-full rounded-xl px-4 py-2">
+                <option value="Filter One">Filter One</option>
+            </select>
+        </div>
+    </div>
+</x-app-layout>
