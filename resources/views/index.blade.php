6ce00fd975b6a6e7f59de89a25ddd56025138c9b
Mark <markjuliusuy@gmail.com>
2021-11-24 13:45:18 +0800

    Episode 3 - Design Idea Cards

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,6 +20,52 @@
                 </svg>
             </div>
         </div>
-        .
     </div>
+
+    <div class="ideas-container space-y-6 my-6">
+        <div class="idea-container bg-white rounded-xl flex hover:shadow-card transition duration-150 ease-in cursor-pointer">
+            <div class="border-r border-gray-100 px-5 py-8">
+                <div class="text-center">
+                    <div class="font-semibold text-2xl">12</div>
+                    <div class="text-gray-500">Votes</div>
+                </div>
+
+                <div class="mt-8">
+                    <button class="w-20 bg-gray-200 border border-gray-200 hover:border-gray-400 transition duration-150 ease-in font-bold text-xxs uppercase rounded-xl px-4 py-3">Vote</button>
+                </div>
+            </div>
+            <div class="flex px-2 py-6">
+                <a href="#" class="flex-none">
+                    <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar" class="w-14 h-14 rounded-xl"/>
+                </a>
+                <div class="mx-4">
+                    <h4 class="text-xl font-semibold">
+                        <a href="#" class="hover:underline">A random title can go here</a>
+                    </h4>
+                    <div class="text-gray-600 mt-3 line-clamp-3">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum voluptate quisquam consequatur deleniti dolore reprehenderit tempore non. Totam, nobis, ipsam dolore ab praesentium hic rerum ipsum perferendis vitae error cumque! Quae beatae, dolores culpa mollitia repellat vel architecto consectetur in blanditiis impedit illo, pariatur facere minus earum labore incidunt dolorem harum sequi facilis. Voluptatum, ipsa? Quis deserunt et magnam fugit dolorum nisi repellat temporibus totam atque! Totam enim earum quas facere maiores alias asperiores voluptate! Ad expedita totam provident asperiores similique vero alias ducimus culpa beatae facilis dolore nobis ab veniam, maxime voluptatibus dolorem repellendus eius harum nam hic! Illum?
+                    </div>
+                    <div class="flex items-center justify-between mt-6">
+                        <div class="flex items-center text-xs  text-gray-400 font-semibold space-x-2">
+                            <div>10 hours ago</div>
+                            <div>&bull;</div>
+                            <div>Category 1</div>
+                            <div>&bull;</div>
+                            <div class="text-gray-900">3 Comments</div>
+                        </div>
+                        <div class="flex items-center space-x-2">
+                            <div class="bg-gray-200 text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">Open</div>
+                            <button class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 transition duration-150 ease-in py-2 px-4">
+                                <svg fill="currentColor" width="24" height="6"><path d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z" style="color: rgba(163, 163, 163, .5)"></svg>
+                                <ul class="absolute w-44 font-semibold bg-white shadow-dialog rounded-xl py-3 text-left ml-8">
+                                    <li><a href="" class="hover:bg-gray-100 block transition duration 150 ease-in px-5 py-3">Mark as Spam</a></li>
+                                    <li><a href="" class="hover:bg-gray-100 block transition duration 150 ease-in px-5 py-3">Delete Post</a></li>
+                                </ul>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- end idea-container -->
+    </div> <!-- end ideas-container -->
 </x-app-layout>
