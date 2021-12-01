96e715bdb992e4f678845e042618b86dda146e0a
Mark <markjuliusuy@gmail.com>
2021-12-01 11:22:43 +0800

    Episode 13

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Cviebrock\EloquentSluggable\Sluggable;
 
+use App\Models\Category;
+
 class Idea extends Model
 {
     use HasFactory, Sluggable;
@@ -31,5 +33,10 @@ class Idea extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
     }
 }
