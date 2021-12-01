8597b34f3a461a8f311131dc51da6de27db54ce2
Mark <markjuliusuy@gmail.com>
2021-12-01 11:01:17 +0800

    Episode 12 - Gravatar Images

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,13 @@ class User extends Authenticatable
     {
         return $this->hasMany(Idea::class);
     }
+
+    public function getAvatar()
+    {
+        $firstCharacter = $this->email[0];
+        
+        $integerToUse = ord(strtolower($firstCharacter)) - (is_numeric($firstCharacter) ? 21 : 96);
+
+        return 'https://www.gravatar.com/avatar/' . md5($this->email) . '?s=200' . '&d=https://s3.amazonaws.com/laracasts/images/forum/avatars/default-avatar-' . $integerToUse . '.png';
+    }
 }
