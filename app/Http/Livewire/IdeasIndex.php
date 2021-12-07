28430ce55fea509fce9092bc774dfcad1173ad2c
Mark <markjuliusuy@gmail.com>
2021-12-07 15:11:45 +0800

    Episode 29 - Other Filters

--- app/Http/Livewire/IdeasIndex.php
+++ app/Http/Livewire/IdeasIndex.php
@@ -14,10 +14,12 @@ class IdeasIndex extends Component
     use WithPagination;
     public $status = 'All';
     public $category;
+    public $filter;
 
     protected $queryString = [
-        'status' => ['except' => ''],
-        'category'
+        'status',
+        'category',
+        'filter'
     ];
 
     protected $listeners = ['queryStringUpdatedStatus'];
@@ -27,14 +29,23 @@ class IdeasIndex extends Component
         $this->status = request()->status ?? 'All';
     }
 
-    // public function updatingStatus()
-    // {
-    //     $this->resetPage();
-    // }
-
     public function updatingCategory()
     {
         $this->resetPage();
+    }
+
+    public function updatingFilter()
+    {
+        $this->resetPage();
+    }
+
+    public function updatedFilter($newFilter)
+    {
+        if ($this->filter === 'My Ideas') {
+            if (! auth()->check()) {
+                return redirect()->route('login');
+            }
+        }
     }
 
 
@@ -56,6 +67,10 @@ class IdeasIndex extends Component
                     return $query->where('status_id', $statuses->get($this->status));
                 })->when($this->category && $this->category !== 'All Categories', function ($query) use ($categories) {
                     return $query->where('category_id', $categories->pluck('id', 'name')->get($this->category));
+                })->when($this->filter && $this->filter === 'Top Voted', function ($query) {
+                    return $query->orderByDesc('votes_count');
+                })->when($this->filter && $this->filter === 'My Ideas', function ($query) {
+                    return $query->where('user_id', auth()->id());
                 })
                 ->addSelect([
                     'voted_by_user' => Vote::select('id')
